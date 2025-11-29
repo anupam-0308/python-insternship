a4e434710e20f4ffa23ab247a47fca997fab6a6a
anupam-0308 <beinganupam.rky@gmail.com>
2025-11-29 14:03:49 +0530

    Update send_email12.py

--- send_email12.py
+++ send_email12.py
@@ -3,7 +3,7 @@ import ssl
 from email.message import EmailMessage
 
 EMAIL = "beinganupam.rky@gmail.com"
-APP_PASSWORD = " jzic bvzv leqk ktet "
+APP_PASSWORD = "app_password"
 RECEIVER = "ayushakabewakoof@gmail.com"
 
 msg = EmailMessage()
@@ -17,4 +17,5 @@ context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
     server.login(EMAIL, APP_PASSWORD)
+
     server.sendmail(EMAIL, RECEIVER, msg.as_string())
