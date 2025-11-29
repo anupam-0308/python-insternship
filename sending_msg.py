0493593408535c370344953a659b9dcca5ff397e
Anupam <beinganupam.rky@gmail.com>
2025-11-29 16:55:47 +0530

    Update sending_msg.py

--- sending_msg.py
+++ sending_msg.py
@@ -1,16 +1,18 @@
-import twilio
-from twilio.rest import Client
+from twilio.rest import Clie
 
+account_sid = "ACxxxxxxxxxxxxxxxxxxxxxxxxxxxx"    
+auth_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" 
 
-account_sid = "MG389cd9a24cf8ef1ef80945088d038ad4"
-auth_token = "6b4912aea689ae24d52a30376b1314d7"
-TWILIONUMBER = "‪+13203772421‬"
-RECEIVERPHONENUMBER = "‪+917050737117‬"
+TWILIO_NUMBER = "+13203772421"        
+RECEIVER_PHONENUMBER = "+917050737117"  
+
 client = Client(account_sid, auth_token)
+
 message = client.messages.create(
     body="Hello from Twilio!",
-    from_=TWILIONUMBER,
-    to=RECEIVERPHONENUMBER
+    from_=TWILIO_NUMBER,
+    to=RECEIVER_PHONENUMBER
 )
-print(f"Message sent with SID: {message.sid}")
-print("Email and SMS sent successfully!")
+
+print("Message sent with SID:", message.sid)
+print("SMS sent successfully!")
