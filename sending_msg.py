30012effdf30fb899f0f51d54f68486067fd123d
Anupam <beinganupam.rky@gmail.com>
2025-11-29 16:57:04 +0530

    Update sending_msg.py

--- sending_msg.py
+++ sending_msg.py
@@ -1,7 +1,7 @@
-from twilio.rest import Clie
+from twilio.rest import Client
 
-account_sid = "ACxxxxxxxxxxxxxxxxxxxxxxxxxxxx"    
-auth_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" 
+account_sid = "xxxx"    
+auth_token = "xxxx" 
 
 TWILIO_NUMBER = "+13203772421"        
 RECEIVER_PHONENUMBER = "+917050737117"  
@@ -16,3 +16,4 @@ message = client.messages.create(
 
 print("Message sent with SID:", message.sid)
 print("SMS sent successfully!")
+
